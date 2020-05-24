48cf122f362634386a9eb2bcac2e2827bcdf84a3
gabriel <gbp@ic.ufal.br>
2020-05-24 12:39:02 -0300

    make responsive adjustments #17

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChildren} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent {
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
+  getWrapperHeight() {
+    const items = this.el.nativeElement.querySelectorAll('.carousel__item');
+    let maxItem = 0;
+    for (const item of items) {
+      if (item.clientHeight > maxItem) {
+        maxItem = item.clientHeight;
+      }
+    }
+    return maxItem + 'px';
   }
-
 }
